856721520753842ad2800f6c25868a7d3f8262a5
tomaszuk <daniel.tomaszuk@netguru.com>
2021-05-07 18:01:13 +0200

    Add backdrop filter with SVG example

---
+++ backdrop_filter_with_svg/static/js/dragElement.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  $( function() {
+    $( "#first" ).draggable();
+    $( "#second" ).draggable();
+    $( "#third" ).draggable();
+  } );
+});
